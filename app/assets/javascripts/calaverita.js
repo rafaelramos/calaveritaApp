d7daa40d2a90b5834cba88d14dde0642f385225d
rafael ramos <rafa-el14@hotmail.com>
2015-01-28 11:00:41 -0600

    Added ornament images and color variable

--- app/assets/javascripts/calaverita.js
+++ app/assets/javascripts/calaverita.js
@@ -6,87 +6,92 @@ calaverita.factory('Elements', function() {
   var Elements = {};
 
   Elements.backgrounds = [
-    { id: 1, src: 'background_1.jpg' },
-    { id: 2, src: 'background_2.jpg' },
-    { id: 3, src: 'background_3.jpg' },
-    { id: 4, src: 'background_4.jpg' },
-    { id: 5, src: 'background_5.jpg' }
+    { id: 1, src: 'background_1.jpg', color: false },
+    { id: 2, src: 'background_2.jpg', color: true },
+    { id: 3, src: 'background_3.jpg', color: false },
+    { id: 4, src: 'background_4.jpg', color: true },
+    { id: 5, src: 'background_5.jpg', color: false }
   ];
 
   Elements.haircuts = [
-    { id: 1, src: 'haircut0001.png' },
-    { id: 2, src: 'haircut0002.png' },
-    { id: 3, src: 'haircut0003.png' },
-    { id: 4, src: 'haircut0004.png' },
-    { id: 5, src: 'haircut0005.png' },
-    { id: 6, src: 'haircut0006.png' },
-    { id: 7, src: 'haircut0007.png' },
-    { id: 8, src: 'haircut0008.png' },
-    { id: 9, src: 'haircut0009.png' },
-    { id: 10, src: 'haircut0010.png' },
-    { id: 11, src: 'haircut0011.png' },
-    { id: 12, src: 'haircut0012.png' },
-    { id: 13, src: 'haircut0013.png' },
-    { id: 14, src: 'haircut0014.png' },
-    { id: 15, src: 'haircut0015.png' },
-    { id: 16, src: 'haircut0016.png' },
-    { id: 17, src: 'haircut0017.png' },
-    { id: 18, src: 'haircut0018.png' },
-    { id: 19, src: 'haircut0019.png' },
-    { id: 20, src: 'haircut0020.png' },
-    { id: 21, src: 'haircut0021.png' },
-    { id: 22, src: 'haircut0022.png' },
-    { id: 23, src: 'haircut0023.png' },
-    { id: 24, src: 'haircut0024.png' },
-    { id: 25, src: 'haircut0025.png' },
-    { id: 26, src: 'haircut0026.png' },
-    { id: 27, src: 'haircut0027.png' },
-    { id: 28, src: 'haircut0028.png' },
-    { id: 29, src: 'haircut0029.png' },
-    { id: 30, src: 'haircut0030.png' },
-    { id: 31, src: 'haircut0031.png' },
-    { id: 32, src: 'haircut0032.png' },
-    { id: 33, src: 'haircut0033.png' },
-    { id: 34, src: 'haircut0034.png' },
-    { id: 35, src: 'haircut0035.png' },
-    { id: 36, src: 'haircut0036.png' },
-    { id: 37, src: 'haircut0037.png' },
-    { id: 38, src: 'haircut0038.png' },
-    { id: 39, src: 'haircut0039.png' },
-    { id: 40, src: 'haircut0040.png' },
-    { id: 41, src: 'haircut0041.png' },
-    { id: 42, src: 'haircut0042.png' },
-    { id: 43, src: 'haircut0043.png' },
-    { id: 44, src: 'haircut0044.png' },
-    { id: 45, src: 'haircut0045.png' },
-    { id: 46, src: 'haircut0046.png' },
-    { id: 47, src: 'haircut0047.png' },
-    { id: 48, src: 'haircut0048.png' },
-    { id: 49, src: 'haircut0049.png' },
-    { id: 50, src: 'haircut0050.png' },
-    { id: 51, src: 'haircut0051.png' },
-    { id: 52, src: 'haircut0052.png' },
-    { id: 53, src: 'haircut0053.png' },
-    { id: 54, src: 'haircut0054.png' },
-    { id: 55, src: 'haircut0055.png' },
-    { id: 56, src: 'haircut0056.png' },
-    { id: 57, src: 'haircut0057.png' },
-    { id: 58, src: 'haircut0058.png' },
-    { id: 59, src: 'haircut0059.png' },
-    { id: 60, src: 'haircut0060.png' },
-    { id: 61, src: 'haircut0061.png' },
-    { id: 62, src: 'haircut0062.png' },
-    { id: 63, src: 'haircut0063.png' },
-    { id: 64, src: 'haircut0064.png' },
-    { id: 65, src: 'haircut0065.png' }
+    { id: 1, src: 'haircut0001.png', color: false },
+    { id: 2, src: 'haircut0002.png', color: true },
+    { id: 3, src: 'haircut0003.png', color: true },
+    { id: 4, src: 'haircut0004.png', color: true },
+    { id: 5, src: 'haircut0005.png', color: true },
+    { id: 6, src: 'haircut0006.png', color: true },
+    { id: 7, src: 'haircut0007.png', color: true },
+    { id: 8, src: 'haircut0008.png', color: true },
+    { id: 9, src: 'haircut0009.png', color: true },
+    { id: 10, src: 'haircut0010.png', color: true },
+    { id: 11, src: 'haircut0011.png', color: true },
+    { id: 12, src: 'haircut0012.png', color: true },
+    { id: 13, src: 'haircut0013.png', color: true },
+    { id: 14, src: 'haircut0014.png', color: true },
+    { id: 15, src: 'haircut0015.png', color: true },
+    { id: 16, src: 'haircut0016.png', color: true },
+    { id: 17, src: 'haircut0017.png', color: true },
+    { id: 18, src: 'haircut0018.png', color: true },
+    { id: 19, src: 'haircut0019.png', color: true },
+    { id: 20, src: 'haircut0020.png', color: true },
+    { id: 21, src: 'haircut0021.png', color: true },
+    { id: 22, src: 'haircut0022.png', color: true },
+    { id: 23, src: 'haircut0023.png', color: true },
+    { id: 24, src: 'haircut0024.png', color: true },
+    { id: 25, src: 'haircut0025.png', color: true },
+    { id: 26, src: 'haircut0026.png', color: true },
+    { id: 27, src: 'haircut0027.png', color: true },
+    { id: 28, src: 'haircut0028.png', color: true },
+    { id: 29, src: 'haircut0029.png', color: true },
+    { id: 30, src: 'haircut0030.png', color: true },
+    { id: 31, src: 'haircut0031.png', color: true },
+    { id: 32, src: 'haircut0032.png', color: true },
+    { id: 33, src: 'haircut0033.png', color: true },
+    { id: 34, src: 'haircut0034.png', color: true },
+    { id: 35, src: 'haircut0035.png', color: true },
+    { id: 36, src: 'haircut0036.png', color: true },
+    { id: 37, src: 'haircut0037.png', color: true },
+    { id: 38, src: 'haircut0038.png', color: true },
+    { id: 39, src: 'haircut0039.png', color: true },
+    { id: 40, src: 'haircut0040.png', color: true },
+    { id: 41, src: 'haircut0041.png', color: true },
+    { id: 42, src: 'haircut0042.png', color: true },
+    { id: 43, src: 'haircut0043.png', color: true },
+    { id: 44, src: 'haircut0044.png', color: true },
+    { id: 45, src: 'haircut0045.png', color: true },
+    { id: 46, src: 'haircut0046.png', color: true },
+    { id: 47, src: 'haircut0047.png', color: true },
+    { id: 48, src: 'haircut0048.png', color: true },
+    { id: 49, src: 'haircut0049.png', color: true },
+    { id: 50, src: 'haircut0050.png', color: true },
+    { id: 51, src: 'haircut0051.png', color: true },
+    { id: 52, src: 'haircut0052.png', color: true },
+    { id: 53, src: 'haircut0053.png', color: true },
+    { id: 54, src: 'haircut0054.png', color: true },
+    { id: 55, src: 'haircut0055.png', color: true },
+    { id: 56, src: 'haircut0056.png', color: true },
+    { id: 57, src: 'haircut0057.png', color: true },
+    { id: 58, src: 'haircut0058.png', color: true },
+    { id: 59, src: 'haircut0059.png', color: true },
+    { id: 60, src: 'haircut0060.png', color: true },
+    { id: 61, src: 'haircut0061.png', color: true },
+    { id: 62, src: 'haircut0062.png', color: true },
+    { id: 63, src: 'haircut0063.png', color: true },
+    { id: 64, src: 'haircut0064.png', color: true },
+    { id: 65, src: 'haircut0065.png', color: true }
   ];
 
   Elements.ornaments = [
-    { id: 1, src: 'ornament_1.png' },
-    { id: 2, src: 'ornament_2.png' },
-    { id: 3, src: 'ornament_3.png' },
-    { id: 4, src: 'ornament_4.png' },
-    { id: 5, src: 'ornament_5.png' }
+    { id: 1, src: 'ornaments0001.png', color: true },
+    { id: 2, src: 'ornaments0002.png', color: true },
+    { id: 3, src: 'ornaments0003.png', color: true },
+    { id: 4, src: 'ornaments0004.png', color: true },
+    { id: 5, src: 'ornaments0005.png', color: true },
+    { id: 6, src: 'ornaments0006.png', color: true },
+    { id: 7, src: 'ornaments0007.png', color: false },
+    { id: 8, src: 'ornaments0008.png', color: true },
+    { id: 9, src: 'ornaments0009.png', color: true },
+    { id: 10, src: 'ornaments0010.png', color: true }
   ];
 
   return Elements;
@@ -98,7 +103,7 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
   $scope.selectedElement = 0;
   $scope.focusedElement = -1;
   $scope.myCalaverita = {
-    background: { backgroundID: 1 },
+    background: { backgroundID: 1, color: { h: 180, s: 100, l: 0, r: 128, g: 128, b: 128 } },
     haircut: { hairID: 1, color: { h: 180, s: 100, l: 0, r: 128, g: 128, b: 128 } },
     ornaments: []
   };
@@ -210,23 +215,6 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
 
         //Update canvas
         $scope.updateCanvas();
-      },
-      onShow: function() {
-        //Set te color selected in the item selected
-        //Get the actual color
-        //var color = { h:180, s:50, b:50},
-        //    colorObj = {};
-        //if ($scope.focusedElement > 0) {
-        //  if ($scope.focusedElement == 999) {
-        //    colorObj = $scope.myCalaverita.haircut.color;
-        //  } else {
-        //    colorObj = getOrnamentFromArray($scope.focusedElement);
-        //  }
-        //  color = { h: colorObj.h, s: colorObj.s, b: colorObj.l }
-        //}
-        //
-        //debugger;
-        //$(this).colPickSetColor();
       }
     });
 
@@ -251,9 +239,8 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
   //Get the background image
   function getBackground() {
     var obj = searchInObject($scope.myCalaverita.background.backgroundID, $scope.elements.backgrounds);
-    if ($scope.myCalaverita.background.colorID > 0) {
-      var rgbObject = searchInObject($scope.myCalaverita.background.colorID, $scope.elements.colors).code;
-      getImageWithColor($scope.stage.getChildAt(0), 'assets/resources/normal/backgrounds/' + obj.src, rgbObject);
+    if (obj.color) {
+      getImageWithColor($scope.stage.getChildAt(0), 'assets/resources/normal/backgrounds/' + obj.src, $scope.myCalaverita.background.color);
     } else {
       getImage($scope.stage.getChildAt(0), 'assets/resources/normal/backgrounds/' + obj.src);
     }
@@ -270,7 +257,11 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
 
     if (hairCutObj.hairID > 1) {
       var obj = searchInObject(hairCutObj.hairID, $scope.elements.haircuts);
-      getImageWithColor($scope.stage.getChildAt(2), 'assets/resources/normal/haircuts/' + obj.src, hairCutObj.color);
+      if (obj.color) {
+        getImageWithColor($scope.stage.getChildAt(2), 'assets/resources/normal/haircuts/' + obj.src, hairCutObj.color);
+      } else {
+        getImage($scope.stage.getChildAt(2), 'assets/resources/normal/haircuts/' + obj.src);
+      }
     }
   }
 
@@ -280,22 +271,17 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
 
     for (var i = 0; i < $scope.myCalaverita.ornaments.length; i++) {
       var obj = searchInObject($scope.myCalaverita.ornaments[i].id, $scope.elements.ornaments);
-      getImageWithColor(objects, 'assets/resources/normal/ornaments/' + obj.src, $scope.myCalaverita.ornaments[i].color);
+      if (obj.color) {
+        getImageWithColor(objects, 'assets/resources/normal/ornaments/' + obj.src, $scope.myCalaverita.ornaments[i].color);
+      } else {
+        getImage(objects, 'assets/resources/normal/ornaments/' + obj.src);
+      }
     }
 
     $scope.stage.getChildAt(3).addChild(objects);
   }
 
   //HELPERS
-
-  function getOrnamentFromArray(id) {
-    for (var i = 0; i < $scope.myCalaverita.ornaments.length; i++) {
-      if ($scope.myCalaverita.ornaments[i].id == id) {
-        return $scope.myCalaverita.ornaments[i];
-      }
-    }
-  };
-
   function updateOrnamentColor(id, color) {
     for (var i = 0; i < $scope.myCalaverita.ornaments.length; i++) {
       if ($scope.myCalaverita.ornaments[i].id == id) {
@@ -332,7 +318,7 @@ calaverita.controller('ElementsCtrl', function($scope, Elements) {
 
   //Get image with color
   function getImageWithColor(container, src, colorObject) {
-    var tint = new createjs.ColorFilter(0, 0, 0, 1, colorObject.r, colorObject.g, colorObject.b, 0);
+    var tint = new createjs.ColorFilter(1, 1, 1, 1, colorObject.r, colorObject.g, colorObject.b, 1);
     var img = new createjs.Bitmap(src);
 
     img.filters = [ tint ];
